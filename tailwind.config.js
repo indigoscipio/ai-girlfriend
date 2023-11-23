8bb6839bed957787c6871159215cf862a650bdf8
Samuel Oktavianus <samueloctavianus97@gmail.com>
2023-11-23 11:09:26 +0700

    fourth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss("./tailwind.config.js")],
+});
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
