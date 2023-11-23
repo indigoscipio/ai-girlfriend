fe4abcd07af1fa15451950c1a5ad89e4d8c08057
Samuel Oktavianus <samueloctavianus97@gmail.com>
2023-11-23 11:36:26 +0700

    gewa

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    cssCodeSplit: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // This ensures the CSS is a separate chunk with a specific filename
-          output: ["dist/output-*.css"],
-        },
-      },
-    },
-  },
 });
