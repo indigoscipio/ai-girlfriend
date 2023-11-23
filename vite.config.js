9b8b61b06eb0a60d1410bf3724721e9f300b4189
Samuel Oktavianus <samueloctavianus97@gmail.com>
2023-11-23 11:34:11 +0700

    xxxx

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // This ensures the CSS is a separate chunk with a specific filename
+          output: ["dist/output-*.css"],
+        },
+      },
+    },
+  },
+});
