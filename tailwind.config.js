664f71f7b606f6e11d6a7c0fe02e360009dcf6d4
Samuel Oktavianus <samueloctavianus97@gmail.com>
2023-11-23 10:38:48 +0700

    third

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}", "./src/components/**/*.{js,ts,jsx}"],
+  content: [
+    "./src/**/*.{html,js,jsx}",
+    "./src/components/**/*.{html, js,ts,jsx}",
+  ],
+  purge: ["./components/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
     extend: {},
   },
